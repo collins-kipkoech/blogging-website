444d508092238fe7a07c5be9ae6febce422d80fe
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-10-31 18:56:12 +0300

    added the registration and login form

---
+++ forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired])
+    confirm_password = PasswordField('Confirm_Password',validators=[DataRequired, EqualTo('password')])
+    submit = SubmitField('Sign up')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    
+    password = PasswordField('Password',validators=[DataRequired])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Login')
+
