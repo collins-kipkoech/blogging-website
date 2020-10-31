101f86599141f365b2bf2f2c113e99a66a2b85b5
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-10-31 19:03:35 +0300

    added register and login routes and view functions

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template,url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = '1b8ab4fce6816adf29e1438f26da19bd'
 
 posts = [
     {
@@ -23,6 +24,15 @@ posts = [
 def index():
     title = 'Blogging Website'
     return render_template('index.html',posts=posts,title=title)
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
