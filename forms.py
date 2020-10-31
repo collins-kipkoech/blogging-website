a621daec38132fae508f9f63c6f2ff9cb6d3a33e
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-10-31 23:01:13 +0300

    added template functionality

--- forms.py
+++ forms.py
@@ -5,15 +5,15 @@ from wtforms.validators import DataRequired, Email, EqualTo
 class RegistrationForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password',validators=[DataRequired])
-    confirm_password = PasswordField('Confirm_Password',validators=[DataRequired, EqualTo('password')])
-    submit = SubmitField('Sign up')
+    password = PasswordField('Password',validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm_Password',validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
     
-    password = PasswordField('Password',validators=[DataRequired])
-    remember = BooleanField('Remember me')
+    password = PasswordField('Password',validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template,url_for, flash,redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -26,13 +26,16 @@ def index():
     return render_template('index.html',posts=posts,title=title)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
     form = RegistrationForm()
     title = 'Registration form'
+    if form.validate_on_submit():
+        flash(f'Account Created successfully','success')
+        return redirect(url_for('index'))
     return render_template('register.html',title=title,form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
     title = 'Login Here'
